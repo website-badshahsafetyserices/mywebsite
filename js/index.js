5fc067941011722bcd5d4835a0f9c822c9d8319c
website-badshahsafetyserices <64730727+website-badshahsafetyserices@users.noreply.github.com>
2020-05-17 14:43:25 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -43,31 +43,89 @@ window.onload = function() {
 
 
 
-    const input_file = "https://github.com/website-badshahsafetyserices/mywebsite/raw/master/badshahsafetyservices.xlsx"
 
-    //const input_file = "http://qcdn.panopath.com/test-2016-4-25.xlsx";
+    /* this is second test code */
 
-    let oReq = new XMLHttpRequest();
-    oReq.open("GET", input_file);
+
+    // const input_file = "https://github.com/website-badshahsafetyserices/mywebsite/raw/master/badshahsafetyservices.xlsx"
+
+    // //const input_file = "http://qcdn.panopath.com/test-2016-4-25.xlsx";
+
+    // let oReq = new XMLHttpRequest();
+    // oReq.open("GET", input_file);
+    // oReq.responseType = "arraybuffer";
+    // oReq.setRequestHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    // oReq.withCredentials = true;
+
+
+    // oReq.onload = function(e) {
+    //     let arraybuffer = oReq.response;
+    //     /*             not responseText!!              */
+
+    //     /* convert data to binary string */
+    //     let data = new Uint8Array(arraybuffer);
+    //     let arr = new Array();
+    //     for (let i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+    //     let bstr = arr.join("");
+    //     /* Call XLSX */
+    //     let workbook = XLSX.read(bstr, { type: "binary" });
+
+    //     //rough start
+    //     workbook.SheetNames.forEach(function(sheetName) {
+    //         // Here is your object
+    //         var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+    //         var json_object = JSON.stringify(XL_row_object);
+    //         console.log(JSON.parse(json_object));
+    //         // jQuery('#xlx_json').val(json_object);
+    //         let div = document.createElement('div');
+    //         div.textContent = json_object;
+
+    //         document.getElementById("xlx_json").appendChild(div);
+    //     })
+
+
+    //     //rough end
+
+
+
+    //     // /* DO SOMETHING WITH workbook HERE */
+    //     // let firstSheet = workbook.SheetNames[0];
+    //     // //document.write(JSON.stringify(workbook));
+    //     // //let parsed = XLSX.utils.sheet_to_csv(firstSheet);
+    //     // let parsed = XLSX.utils.sheet_to_row_object_array(firstSheet);
+    //     // document.getElementById("xlx_json").textContent = parsed;
+    //     // console.log(parsed)
+
+    // };
+
+    // oReq.send();
+
+
+    /* this is third code */
+
+    var url = "badshahsafetyservices.xlsx";
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", url, true);
     oReq.responseType = "arraybuffer";
-    oReq.setRequestHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    //oReq.withCredentials = true;
-
-
 
     oReq.onload = function(e) {
-        let arraybuffer = oReq.response;
-        /*             not responseText!!              */
+        var arraybuffer = oReq.response;
 
         /* convert data to binary string */
-        let data = new Uint8Array(arraybuffer);
-        let arr = new Array();
-        for (let i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-        let bstr = arr.join("");
-        /* Call XLSX */
-        let workbook = XLSX.read(bstr, { type: "binary" });
+        var data = new Uint8Array(arraybuffer);
+        var arr = new Array();
+        for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+        var bstr = arr.join("");
 
-        //rough start
+        /* Call XLSX */
+        var workbook = XLSX.read(bstr, { type: "binary" });
+
+        /* DO SOMETHING WITH workbook HERE */
+        var first_sheet_name = workbook.SheetNames[0];
+        /* Get worksheet */
+        var worksheet = workbook.Sheets[first_sheet_name];
+        // console.log(XLSX.utils.sheet_to_json(worksheet, { raw: true }));
+
         workbook.SheetNames.forEach(function(sheetName) {
             // Here is your object
             var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
@@ -81,21 +139,16 @@ window.onload = function() {
         })
 
 
-        //rough end
 
 
 
-        // /* DO SOMETHING WITH workbook HERE */
-        // let firstSheet = workbook.SheetNames[0];
-        // //document.write(JSON.stringify(workbook));
-        // //let parsed = XLSX.utils.sheet_to_csv(firstSheet);
-        // let parsed = XLSX.utils.sheet_to_row_object_array(firstSheet);
-        // document.getElementById("xlx_json").textContent = parsed;
-        // console.log(parsed)
+        // let div = document.createElement('div');
+        // div.textContent = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+        // document.getElementById("xlx_json").appendChild(div);
 
-    };
+    }
 
-    oReq.send({ 'request': "authentication token" });
+    oReq.send();
 
 
 }
